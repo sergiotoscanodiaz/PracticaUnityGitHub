59ca6c010549c7e8e9bc3b18306e2834fad0f950
Sergio Toscano Díaz <55782014+sergiotoscanodiaz@users.noreply.github.com>
2022-01-26 13:54:55 +0100

    Pruebas

---
+++ Assets/Scripts/SueloMortalmenteInvisible.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SueloMortalmenteInvisible : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    //Update no es necesario
+}
